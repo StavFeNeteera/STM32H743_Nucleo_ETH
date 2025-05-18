7d8f4d00ac2fd298464bdee193d53ebb56a36379
StavFeNeteera <stav.ferrera@neteera.com>
2025-05-18 13:44:28 +0300

    added TCP server
    connect to port 1234on cmd:
    telnet <board_ip> 1234
    then send msgs over ethernet

--- App/app.c
+++ App/app.c
@@ -1,10 +1,52 @@
 #include "app.h"
 #include <stdio.h>
+#include "lwip/tcp.h"
+#include "lwip/ip_addr.h"
+#include "lwip/tcpip.h"
+
+#define TCP_SERVER_PORT 1234
+static struct tcp_pcb *tcp_server_pcb = NULL;
+
+static err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
+{
+    if (p != NULL) {
+        printf("Message received: %.*s\r\n", p->len, (char*)p->payload);
+        tcp_recved(tpcb, p->len);
+        pbuf_free(p);
+    }
+    return ERR_OK;
+}
+
+static err_t tcp_server_accept(void *arg, struct tcp_pcb *newpcb, err_t err)
+{
+    printf("TCP connection made\r\n");
+    tcp_recv(newpcb, tcp_server_recv);
+    return ERR_OK;
+}
+
+void tcp_server_init(void)
+{
+    printf("Trying to set TCP server\r\n");
+    tcp_server_pcb = tcp_new();
+    if (tcp_server_pcb != NULL) {
+        err_t err = tcp_bind(tcp_server_pcb, IP_ADDR_ANY, TCP_SERVER_PORT);
+        if (err == ERR_OK) {
+            tcp_server_pcb = tcp_listen(tcp_server_pcb);
+            tcp_accept(tcp_server_pcb, tcp_server_accept);
+            printf("TCP is up, listening on port %d\r\n", TCP_SERVER_PORT);
+        } else {
+            printf("TCP bind failed\r\n");
+        }
+    } else {
+        printf("TCP PCB allocation failed\r\n");
+    }
+}
 
 void app_init(void)
 {
     /* init code for LWIP */
     MX_LWIP_Init();
+    tcpip_callback((tcpip_callback_fn)tcp_server_init, NULL);
 }
 
 void app_run(void *argument)
